1bd9689ed5ac9fc3ee38d8443a486bf9e6474cfe
dansun2 <166355466+dansun2@users.noreply.github.com>
2025-03-20 09:29:45 +0900

    [Bronze III] Title: 최소, 최대, Time: 1588 ms, Memory: 318480 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] number = new int[n];
+
+        for (int i=0;i<number.length;i++){
+            number[i] = sc.nextInt();
+        }
+        sc.close();
+        
+        int min = number[0];
+        int max = number[0];
+        
+        for (int i=1;i<number.length;i++){
+            min = Math.min(min,number[i]);
+            max = Math.max(max,number[i]);
+        }
+        System.out.println(min + " " + max);
+    }
+}
